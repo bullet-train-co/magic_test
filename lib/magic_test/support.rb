58db2438e7f033ebc825a76c47ceeb45ea1a8948
Andrew Culver <andrew.culver@gmail.com>
2021-03-01 14:53:30 -0500

    Update support.rb

--- lib/magic_test/support.rb
+++ lib/magic_test/support.rb
@@ -107,7 +107,6 @@ module MagicTest
 
     # TODO this feels like it's going to end up burning people who have other support files in `test` or `spec` that don't include `helper` in the name.
     def get_last_caller(caller)
-      binding.pry
       caller select { |s| s.include?("/test/") || s.include?("/spec/") }.reject { |s| s.include?("helper") }
     end
   end
