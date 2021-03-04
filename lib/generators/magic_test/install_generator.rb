719757513f92c41267006305cfb543d03f0d395e
Louis Sommer <louis.sommer@gmail.com>
2021-03-04 10:29:25 +0100

    install generator now inserts the required line in all html layouts. Also protected the basic test replacement in case user doesn't use Minitest

--- lib/generators/magic_test/install_generator.rb
+++ lib/generators/magic_test/install_generator.rb
@@ -15,6 +15,21 @@ module MagicTest
       end
 
       generate "system_test", "basic"
+      replace_test if File.exists?("tests/system/basic_test.rb")
+      insert_in_layout
+    end
+
+    private
+
+    def insert_in_layout
+      files = layouts
+      string = "    <%= render 'magic_test/support' if Rails.env.test? %>\n"
+      files.each do |file|
+        insert_into_file file, string, before: /^\s*<\/head>/
+      end
+    end
+
+    def replace_test
       gsub_file "test/system/basics_test.rb", "# ", ""
       gsub_file "test/system/basics_test.rb", "#", ""
       gsub_file "test/system/basics_test.rb", "visiting the index", "getting started"
@@ -23,6 +38,11 @@ module MagicTest
 
       gsub_file "test/application_system_test_case.rb", "using: :headless_chrome", "using: (ENV['SHOW_TESTS'] ? :chrome : :headless_chrome)"
       gsub_file "test/application_system_test_case.rb", "using: :headless_firefox", "using: (ENV['SHOW_TESTS'] ? :firefox : :headless_firefox)"
+    end
+
+    def layouts
+      files = Dir.glob("#{Rails.root}/app/views/layouts/**/*.html.erb")
+      files.reject { |file| file =~ /mailer/ }
     end
   end
 end
