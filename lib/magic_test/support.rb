d3dc97fcd43bfe29da5ce48211ca9a7ba4eca47a
gazayas <g-zayas@hotmail.com>
2023-06-23 11:28:21 +0900

    Fixing Standard Ruby

--- lib/magic_test/support.rb
+++ lib/magic_test/support.rb
@@ -6,7 +6,7 @@ module MagicTest
 
       filepath, line = get_last_caller(caller)
 
-      contents = File.open(filepath).read.lines
+      contents = File.read(filepath).lines
       chunks = contents.each_slice(line.to_i - 1 + @test_lines_written).to_a
       indentation = chunks[1].first.match(/^(\s*)/)[0]
       chunks.first << indentation + "assert(page.has_content?('#{selected_text.gsub("'", "\\\\'")}'))" + "\n"
@@ -24,7 +24,7 @@ module MagicTest
     def flush
       filepath, line = get_last_caller(caller)
 
-      contents = File.open(filepath).read.lines
+      contents = File.read(filepath).lines
       chunks = contents.each_slice(line.to_i - 1 + @test_lines_written).to_a
       indentation = chunks[1].first.match(/^(\s*)/)[0]
       output = page.evaluate_script("JSON.parse(sessionStorage.getItem('testingOutput'))")
@@ -54,7 +54,7 @@ module MagicTest
       filepath, line = get_last_caller(caller)
 
       puts "(writing that to `#{filepath}`.)"
-      contents = File.open(filepath).read.lines
+      contents = File.read(filepath).lines
       chunks = contents.each_slice(line.to_i - 1 + @test_lines_written).to_a
       indentation = chunks[1].first.match(/^(\s*)/)[0]
       get_last.each do |last|
@@ -91,11 +91,11 @@ module MagicTest
 
     def magic_test_pry_hook
       Pry.hooks.add_hook(:before_session, "magic_test") do |output, binding, pry|
-        Pry.hooks.delete_hook(:before_session, 'magic_test')
-        magic_test_file_index = pry.backtrace.index{|line| line.include?(__FILE__)}
+        Pry.hooks.delete_hook(:before_session, "magic_test")
+        magic_test_file_index = pry.backtrace.index { |line| line.include?(__FILE__) }
         # walk up backtrace until finding the original caller
-        until pry.backtrace[magic_test_file_index + 1].include?(pry.last_file) do
-          pry.run_command('up')
+        until pry.backtrace[magic_test_file_index + 1].include?(pry.last_file)
+          pry.run_command("up")
         end
       end
     end
--- test/dummy/test/application_system_test_case.rb
+++ test/dummy/test/application_system_test_case.rb
@@ -1,5 +1,5 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: (ENV['MAGIC_TEST'] ? :chrome : :headless_chrome), screen_size: (ENV['MAGIC_TEST'] ? [800, 1400] : [1400, 1400])
+  driven_by :selenium, using: (ENV["MAGIC_TEST"] ? :chrome : :headless_chrome), screen_size: (ENV["MAGIC_TEST"] ? [800, 1400] : [1400, 1400])
 end
